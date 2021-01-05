c92b7d31e247e0807ee40057ed339b21e58c98bc
Bryce B <bryce.boddy@gmail.com>
2021-01-05 07:05:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ function moveDodgerRight(){
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10)
 }
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+})
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
+})
