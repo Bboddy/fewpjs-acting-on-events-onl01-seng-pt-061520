3bd33d6bf68206594bbdbce31d089071cad3149c
Bryce B <bryce.boddy@gmail.com>
2021-01-05 07:05:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ var dodger = document.getElementById('dodger');
 dodger.style.backgroundColor = "#FF69B4";
 
 function moveDodgerLeft() {
-
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
 }
 
 function moveDodgerRight(){
-
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var left = parseInt(leftNumbers, 10)
 }
